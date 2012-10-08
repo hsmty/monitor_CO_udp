cc4c002bb28778a0014018d23f799c568bf09be0
R. Diaz de Leon <leon@elinter.net>
2012-10-08 01:45:37 -0500

    Reestructured the code and fixed a buffer overflow

--- CO_ethernet_UDP/CO_ethernet_UDP.ino
+++ CO_ethernet_UDP/CO_ethernet_UDP.ino
@@ -1,48 +1,37 @@
 #include <SPI.h>         // needed for Arduino versions later than 0018
 #include <Ethernet.h>
 #include <EthernetUdp.h>     // UDP library from: bjoern@cs.stanford.edu 12/30/2008
-
-int analogPin = 0;
+#include <string.h>
 
 // Enter a MAC address and IP address for your controller below.
 // The IP address will be dependent on your local network:
 byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 IPAddress server(66, 228, 50, 204);
 IPAddress address(192, 168, 1, 201);
-unsigned int port = 2323, packetSize, secs = 5000; // UDP port on server, removi la variable chackin
+// UDP port on server
+unsigned int port = 2323, packetSize, secs = 5000;
+EthernetUDP udp; //Ethernet UDP interface
 
-unsigned int device = 0; // variable para diferenciar los distintos dispositivos
+unsigned int device = 0; // Device id
+int lastRead = -1; // Used to only update when the reading changes
+int meterPin = 0;
 
-int regLectura; // registro para monitoreo del valor de la lectura
-
-
-/* Buffer to hold incoming packet, utilizo recibido y command globales para poder obtener
- un char array como resultado de las funciones recepcion() y hacerString()*/
-char packetBuffer[UDP_TX_PACKET_MAX_SIZE],recibido[7],command[7];
-
-
-// An EthernetUDP instance to let us send and receive packets over UDP
-EthernetUDP Udp;
-
-void setup() {
+void setup() 
+{
   Serial.begin(9600);
-  delay(1000);
-  Serial.println("Iniciando DHCOP...");
+  Serial.println("running setup");
+  Serial.println("Iniciando DHCP...");
   // Request DHCP for address or use default
   if (Ethernet.begin(mac)) {
     Serial.print("Obtenida IP: ");
     Serial.println(Ethernet.localIP());
-  } 
-  else {
+  } else {
     Serial.print("DHCP fallo, asignando statica: ");
     Ethernet.begin(mac, address);
-    delay(500);
     Serial.println(address);
   }
     
-  Udp.begin(port);
- 
-  
+  udp.begin(port);  
 }
 
 /* en loop creamos una variable que sirve de registro del valor de lectura,
@@ -50,23 +39,16 @@ de esta forma solo hacemos posts cuando hay cambio en el valor*/
 
 void loop()
 {
-  
-  int lectura = lecturaCO();
-  if (lectura != regLectura){
-    Serial.println("Cambio en la lectura");
-    Serial.print("Lectura ahora es: ");
-    Serial.println(lectura);
-    hacerString(lectura, device, command);
-    Serial.println("Creando String");
-    Serial.print("String a enviar:");
-    Serial.println(command);
-    delay(1000);
-    Serial.println("Actualizando valores a servidor");
-    envioUDP(server,port,command);
-    recepcion(recibido,command);     
+  int packeSize = 0; 
+  char* command = (char*) malloc(sizeof(char) * 12);
+  int reading = analogRead(meterPin);
+
+  if (reading != lastRead) {
+    sprintf(command, "%03d:%03d:store", reading, device);
+    udpSend(server, port, command);
+    lastRead = reading;
+  } else {
+    Serial.println("Lectura sin cambio");
   }
-  else{
-    Serial.print("Lectura sin cambio");
-    delay(1000);
-}
+  delay(1000);
 }
--- CO_ethernet_UDP/envio_udp.ino
+++ CO_ethernet_UDP/envio_udp.ino
@@ -1,34 +1,24 @@
-  // Send a request for checkin status on our
-  // UDP server
-  
-void envioUDP(IPAddress server, unsigned int port, char command[] )
-  {
-    Serial.println ("Sending request...");
-    Serial.println (server, port);
-    Udp.beginPacket(server, port);
-    Udp.write(command);
-    Udp.endPacket();
-  } 
+// Send a request for checkin status on our
+// UDP server
+char packetBuffer[UDP_TX_PACKET_MAX_SIZE];
  
-void recepcion(char recibido[],char command[]) 
-  {
-  int packetSize = Udp.parsePacket();
-     
-  if (packetSize){
-    IPAddress remote = Udp.remoteIP();
-    // Read the packet into packetBufffer
-    Udp.read(packetBuffer,UDP_TX_PACKET_MAX_SIZE);
-    Serial.println("Recived: ");
-    Serial.println(packetBuffer);
-    recibido = packetBuffer; 
-    if (recibido=command){
-     Serial.println("Envio OK");
-    }else{
-     Serial.println("Envio Difiere");
-    } 
+int udpSend(IPAddress server, unsigned int port, char command[])
+{
+  int ps = 0;// Packet size
+  udp.beginPacket(server, port);
+  udp.write(command);
+  udp.endPacket();
+  
+  ps = udp.parsePacket();
+  if (ps) {
+    udp.read(packetBuffer, UDP_TX_PACKET_MAX_SIZE);
+    // If packetBuffer and command are equal
+    if (strcmp(packetBuffer, command) == 0) {
+      return 1;
+    }
+  } else {
+    Serial.println("No response from server");
   }
-  else{
-    Serial.println("no hubo respuesta");
-  }
-    
-  }
+
+  return 0;
+}
